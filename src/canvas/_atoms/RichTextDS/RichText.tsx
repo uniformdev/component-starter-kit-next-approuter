ce4906abf3710b94a836a45c0e3bf9bcaaaa04cf
Michal Majer <Michal.Majer@StoneX.com>
2024-09-03 17:01:13 +0200

    Add RTE fetching content from K.AI

--- src/canvas/_atoms/RichTextDS/RichText.tsx
+++ src/canvas/_atoms/RichTextDS/RichText.tsx
@@ -1,15 +1,25 @@
 import { FC } from 'react';
 import { RichTextProps } from '.';
-import { createRichTextHtmlResolver } from '@kontent-ai/delivery-sdk';
-// import { nodeParser } from '@kontent-ai/delivery-node-parser';
+import { nodeParser } from '@kontent-ai/delivery-node-parser';
+import { createDeliveryClient, createRichTextHtmlResolver, linkedItemsHelper } from '@kontent-ai/delivery-sdk';
 
-export const RichText: FC<RichTextProps> = ({ text }) => {
-  const resolvedRichText = createRichTextHtmlResolver().resolveRichText({
-    element: text,
+const deliveryClient = createDeliveryClient({
+  environmentId: process.env.KONTENT_AI_ENVIRONMENT_ID || '',
+});
+
+export const RichText: FC<RichTextProps> = async ({ text }) => {
+  const response = await deliveryClient.item<any>('ai_robotic_powered_vehicle_kit_25a0265').toPromise();
+  const richTextElement = response.data.item.elements.content;
+
+  console.log('richTextElement.images', richTextElement.images);
+  console.log('text.images', text.images);
+
+  const resolvedRichText = createRichTextHtmlResolver(nodeParser).resolveRichText({
+    element: richTextElement,
 
     contentItemResolver: (itemId, contentItem) => {
       return {
-        contentItemHtml: `<div>Unsupported type ${contentItem?.system.type} for ${itemId}</div>`,
+        contentItemHtml: `<div class="p-2 border-2 font-bold text-center">Unsupported type ${contentItem?.system.type} for ${itemId}</div>`,
       };
     },
   });

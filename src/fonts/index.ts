562dd0361582d6322334b69b3f3f17565bea0e03
trsergey <stu@dbbsoftware.com>
2025-10-01 15:54:31 +0300

    chore: enhance component pattern and composition

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -27,4 +27,58 @@ const roboto_flex = localFont({
   variable: '--roboto-flex',
 });
 
-export const customFontVariables = [roboto_serif.variable, roboto_flex.variable].join(' ');
+const inter = localFont({
+  src: [
+    {
+      path: './inter/Inter-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './inter/Inter-SemiBold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './inter/Inter-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--inter',
+});
+
+const tiempos = localFont({
+  src: [
+    // {
+    //   path: './inter/TiemposText-RegularItalic.woff2',
+    //   weight: '400',
+    //   style: 'italic',
+    // },
+    {
+      path: './inter/TiemposText-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './inter/TiemposText-Semibold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    // {
+    //   path: './inter/TiemposText-SemiboldItalic.woff2',
+    //   weight: '600',
+    //   style: 'italic',
+    // },
+    {
+      path: './inter/TiemposHeadline-Semibold.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+  ],
+  variable: '--tiempos',
+});
+
+export const customFontVariables = [roboto_serif.variable, roboto_flex.variable, inter.variable, tiempos.variable].join(
+  ' '
+);

c813418f08fe66b0151b9a99bdb7b1cca121319a
trsergey <stu@dbbsoftware.com>
2025-10-02 17:19:13 +0300

    chore: return config files as downloadable attachments.

--- src/app/api/dex-config/route.ts
+++ src/app/api/dex-config/route.ts
@@ -2,14 +2,29 @@ import { NextRequest, NextResponse } from 'next/server';
 import dexConfigNP from '../../../../dex.config.np.json';
 import dexConfigSU from '../../../../dex.config.su.json';
 
+const getFile = (type?: string | null) => {
+  switch (type) {
+    case 'np':
+      return { config: dexConfigNP, filename: 'dex.config.np.json' };
+    case 'su':
+      return { config: dexConfigSU, filename: 'dex.config.su.json' };
+    default:
+      return { config: {}, filename: 'dex.config.json' };
+  }
+};
+
 export const GET = (request: NextRequest) => {
   const { searchParams } = new URL(request.url);
   const type = searchParams.get('type');
-  if (type === 'np') {
-    return NextResponse.json(dexConfigNP);
-  }
-  if (type === 'su') {
-    return NextResponse.json(dexConfigSU);
-  }
-  return NextResponse.json({});
+
+  const { config, filename } = getFile(type);
+
+  const jsonString = JSON.stringify(config, null, 2);
+
+  return new NextResponse(jsonString, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Disposition': `attachment; filename="${filename}"`,
+    },
+  });
 };
--- src/app/api/tokens-config/route.ts
+++ src/app/api/tokens-config/route.ts
@@ -2,14 +2,29 @@ import { NextRequest, NextResponse } from 'next/server';
 import tokensConfigNP from '../../../../np.json';
 import tokensConfigSU from '../../../../su.json';
 
+const getFile = (type?: string | null) => {
+  switch (type) {
+    case 'np':
+      return { config: tokensConfigNP, filename: 'np.json' };
+    case 'su':
+      return { config: tokensConfigSU, filename: 'su.json' };
+    default:
+      return { config: {}, filename: 'tokens.json' };
+  }
+};
+
 export const GET = (request: NextRequest) => {
   const { searchParams } = new URL(request.url);
   const type = searchParams.get('type');
-  if (type === 'np') {
-    return NextResponse.json(tokensConfigNP);
-  }
-  if (type === 'su') {
-    return NextResponse.json(tokensConfigSU);
-  }
-  return NextResponse.json({});
+
+  const { config, filename } = getFile(type);
+
+  const jsonString = JSON.stringify(config, null, 2);
+
+  return new NextResponse(jsonString, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Disposition': `attachment; filename="${filename}"`,
+    },
+  });
 };
